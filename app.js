f0eecedad4401f3af9e54a6c33dbc4271bd835b2
sergiuszlts <userjscpp@gmail.com>
2019-07-17 18:18:07 +0200

    MongoDB integration and loading data from database

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
 app.use(express.static('public'))
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/home.html'));
+});
+
+app.get('/load-json-all', function (req, res) {
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("quiz").find({}, { projection: { _id: 0, title: 1, description: 1, id: 1 } }).toArray(function (err, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+        });
+    });
 });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
---
+++ public/home.js
@@ -0,0 +1,18 @@
+function initial() {
+    fetch("/load-json-all")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (res) {
+            display(res);
+        }).catch(function (error) {
+            document.getElementById("content").innerHTML = "<h4>An error occured :(";
+        });
+}
+
+function display(res) {
+    let cont = document.getElementById("content");
+    res.forEach(quiz => {
+        cont.innerHTML += `<div class="quizlink"><button class="button buttonfull" onclick="window.location.href='/quiz/${quiz.id}'"><h4>${quiz.title}</h4> ${quiz.description}</button></div>`;
+    });
+}
